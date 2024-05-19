fbba76194ee57dec4277e3a4d16cf2bb739fe57f
shema paulin <116367425+shemapaulin@users.noreply.github.com>
2024-05-19 18:20:01 +0200

    Update Routes.jsx

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Garelly from './Garelly';
-import { Route, Routes, useLocation, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes,useLocation } from 'react-router-dom';
 import Home from './Home';
 import About from './Pages/about'
 
@@ -9,15 +9,13 @@ import Contact from './Pages/contact';
 const MainRoutes = () => {
     const location = useLocation();
   return (
-    <Router basename="/">
-      <Routes key={location.pathname} location={location}>
-        <Route path="/" element={<Home/>}/>
-        <Route path="#/garelly" element={<Garelly/>}/>
-        <Route path="#/about" element={<About/>}/>
-        <Route path="#/programs" element={<Programs/>}/>
-        <Route path="#/contact" element={<Contact/>}/>
-      </Routes>
-    </Router>
+    <Routes key={location.pathname} location={location}>
+<Route path="/" element={<Home/>}/>
+    <Route path="/gallery" element={<Garelly/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/programs" element={<Programs/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+     </Routes>
   )
 }
 
