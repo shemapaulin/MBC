5d3b167034c2c335f5bbe9317e68c1a505f4eeea
shema paulin <116367425+shemapaulin@users.noreply.github.com>
2024-05-19 17:12:15 +0200

    Quck check fix Routes.jsx

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,22 +1,41 @@
-import React from 'react'
-import Garelly from './Garelly';
-import { Route, Routes,useLocation } from 'react-router-dom';
+import React from 'react';
+import { useRoutes, useNavigate } from 'react-router-dom';
 import Home from './Home';
 import About from './Pages/about'
-
 import Programs from './Pages/Programs';
 import Contact from './Pages/contact';
-const MainRoutes = () => {
-    const location = useLocation();
-  return (
-    <Routes key={location.pathname} location={location}>
-<Route path="/" element={<Home/>}/>
-    <Route path="/Garelly" element={<Garelly/>}/>
-    <Route path="/About" element={<About/>}/>
-    <Route path="/Programs" element={<Programs/>}/>
-    <Route path="/Contact" element={<Contact/>}/>
-     </Routes>
-  )
-}
 
-export default MainRoutes
+const routes = [
+  { path: '/', element: <Home /> },
+  { path: '/about', element: <About /> },
+  { path: '/programs', element: <Programs /> },
+  { path: '/contact', element: <Contact /> },
+  { path: '/company', element: <About /> },
+];
+
+const MainRoutes = () => {
+  const element = useRoutes(routes);
+  const navigate = useNavigate();
+
+  // If no matching route was found...
+  if (!element) {
+    const path = window.location.pathname;
+
+    // Check if any of the defined paths are included in the current path
+    const routesExcludingLast = routes.slice(0, -1);
+    const similarRoute = routesExcludingLast.find(route => path.includes(route.path));
+
+    // If a similar route was found, navigate to it
+    if (similarRoute) {
+      navigate(similarRoute.path);
+    } 
+    // If no similar route was found, navigate to the home page
+    else {
+      navigate('/');
+    }
+  }
+
+  return element;
+};
+
+export default MainRoutes;
