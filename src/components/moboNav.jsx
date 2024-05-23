c56ad3797616f7f586ffd9a6a2242fc6df6812f3
KWIZERA <43585601+heiskwizera@users.noreply.github.com>
2024-05-23 08:50:46 +0200

    added auto close on mobo nav

--- src/components/moboNav.jsx
+++ src/components/moboNav.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-
-import { CgMenuRight } from "react-icons/cg";
-import { IoMdClose } from "react-icons/io";
-
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { CgMenuRight } from "react-icons/cg";
+import { IoMdClose } from "react-icons/io";
 
 const menuVariants = {
   hidden: {
@@ -20,26 +18,40 @@ const menuVariants = {
 
 const MobileNav = () => {
   const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const closeMenu = () => {
+    setOpenMenu(false);
+  };
+
   return (
     <nav className="xl:hidden text-primary ">
-      <div className="text-3xl cursor-pointer  absolute top-14 right-4" onClick={() => setOpenMenu(true)}>
+      <div className="text-3xl cursor-pointer absolute top-14 right-4" onClick={toggleMenu}>
         <CgMenuRight />
       </div>
-      <motion.div variants={menuVariants} initial="hidden" animate={openMenu ? "show" : ""} className="bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20 ">
-        <div className="text-4xl absolute z-30 left-4 top-4 text-black cursor-pointer text-Primary" onClick={() => setOpenMenu(false)}>
+      <motion.div
+        variants={menuVariants}
+        initial="hidden"
+        animate={openMenu ? "show" : "hidden"} // Close menu if openMenu is false
+        className="bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20"
+      >
+        <div className="text-4xl absolute z-30 left-4 top-4 text-black cursor-pointer text-Primary" onClick={closeMenu}>
           <IoMdClose />
         </div>
         <ul className="h-full flex flex-col justify-center items-center text-black gap-y-8 text-primary font-Primary font-bold text-3xl cursor-pointer">
-          <li>
+          <li onClick={closeMenu}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li onClick={closeMenu}>
             <Link to="/about">About</Link>
           </li>
-          <li>
+          <li onClick={closeMenu}>
             <Link to="/programs">Programs</Link>
           </li>
-          <li>
+          <li onClick={closeMenu}>
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
