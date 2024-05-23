bdf35aef961f5d08766931b0f37072fd3785b343
heiskwizera <kwizerageniklenon@gmail.com>
2024-05-23 10:05:30 +0200

    sticky mobo nav

--- src/components/moboNav.jsx
+++ src/components/moboNav.jsx
@@ -28,15 +28,15 @@ const MobileNav = () => {
   };
 
   return (
-    <nav className="xl:hidden text-primary ">
-      <div className="text-3xl cursor-pointer absolute top-14 right-4" onClick={toggleMenu}>
-        <CgMenuRight />
+    <nav className="xl:hidden md:hidden text-primary fixed top-0 left-0 right-0 z-30">
+      <div className="bg-black mr-1 rounded-full p-1 text-2xl cursor-pointer absolute top-4 right-4 shadow-md" onClick={toggleMenu}>
+        <CgMenuRight color="white" />
       </div>
-      <motion.div variants={menuVariants} initial="hidden" animate={openMenu ? "show" : "hidden"} className="bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20">
-        <div className="text-4xl absolute z-30 left-4 top-4 text-black cursor-pointer text-Primary" onClick={closeMenu}>
+      <motion.div variants={menuVariants} initial="hidden" animate={openMenu ? "show" : "hidden"} className="bg-white shadow-2xl w-full fixed top-0 right-0 max-w-xs h-screen z-20">
+        <div className="bg-white rounded-full p-2 text-4xl absolute z-30 left-4 top-4 text-black cursor-pointer shadow-md" onClick={closeMenu}>
           <IoMdClose />
         </div>
-        <ul className="h-full flex flex-col justify-center items-center text-black gap-y-8 text-primary font-Primary font-bold text-3xl cursor-pointer">
+        <ul className="h-full flex flex-col justify-center items-center text-black gap-y-8 text-primary font-bold text-3xl cursor-pointer">
           <li onClick={closeMenu}>
             <Link to="/">Home</Link>
           </li>
